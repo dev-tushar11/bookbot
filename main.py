9f10bba5a391db9c6ea725aa52f49c4dc2a58c0e
dev-tushar11 <paltushar2001@gmail.com>
2022-10-09 22:49:32 +0530

    count_letters function added

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ def count_words():
     print(len(words))
 
 count_words()
+
+def count_letters():
+    letters = {}
+    words = file_contents.split()
+    for word in words:
+        for letter in word.lower():
+            if letter not in letters:
+                letters[letter] = 1
+            else:
+                letters[letter] = letters[letter] + 1
+    print(letters)
+
+count_letters()
